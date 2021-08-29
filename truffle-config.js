781f488d217971728bea4cf3b697f993a7e5b50d
Alex Sheppert <alecsheppert@gmail.com>
2021-08-28 20:31:44 -0600

    rinkleby push

--- truffle-config.js
+++ truffle-config.js
@@ -69,9 +69,9 @@ module.exports = {
     //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
-    ropsten: {
+    rinkleby: {
       provider: () => new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/ws/v3/1cf5a928f6b54b61853f92ded960792a`),
-      network_id: 3,       // Ropsten's id
+      network_id: 4,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
